0e60ed54ee61d10cb4fb35eaa71de7397ed5a32e
Vicente_Llanos <vicentedavid.llanospaco.contractor@gdevtools.com>
2025-06-01 20:52:45 -0300

    commit

---
+++ app/src/main/java/com/curso/trabajoconoperadores/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.curso.trabajoconoperadores
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.curso.trabajoconoperadores.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val miNumero:EditText = findViewById(R.id.editTextText)
+        val miBoton: Button = findViewById(R.id.button)
+        val miResultado: TextView = findViewById(R.id.textView)
+
+        miBoton.setOnClickListener{
+            val numero: Int? = miNumero.text.toString().toIntOrNull()
+            miResultado.text= if (numero == null) "No es un numero"
+            else if (numero < 18) "Es menor de edad"
+            else if(numero > 18) "Es mayor de edad"
+            else "tienes $numero años"
+        }
+    }
+}
